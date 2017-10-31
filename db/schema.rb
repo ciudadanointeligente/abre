a8ed3ff214397a589c4b492723993a9c00eff433
Dani <dani@localhost.localdomain>
2017-10-31 11:06:02 -0300

    tenants with seed

--- db/schema.rb
+++ db/schema.rb
@@ -927,7 +927,7 @@ ActiveRecord::Schema.define(version: 20171025195353) do
     t.boolean  "email_digest",                              default: true
     t.boolean  "email_on_direct_message",                   default: true
     t.boolean  "official_position_badge",                   default: false
-    t.datetime "password_changed_at",                       default: '2017-07-17 16:03:32', null: false
+    t.datetime "password_changed_at",                       default: '2017-10-31 14:02:13', null: false
     t.boolean  "created_from_signature",                    default: false
     t.integer  "failed_email_digests_count",                default: 0
     t.text     "former_users_data_log",                     default: ""
