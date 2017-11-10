a4ac69ee2bed8284dc1a33188431076fd184b409
daquiroz <daquiroz@uc.cl>
2017-11-10 18:02:19 -0300

    design events view

--- app/controllers/design_events_controller.rb
+++ app/controllers/design_events_controller.rb
@@ -2,6 +2,9 @@ class DesignEventsController < ApplicationController
   skip_authorization_check
 
   def index
+    p params
+    @design_events = Project.find(params[:project_id]).design_events
+
   end
 
 end
