c01a66c8196387a575b856f172f0ba317b1ff1ef
Daniela Quiroz <daquiroz@uc.cl>
2018-04-03 15:44:38 -0300

    Design event in admin panel (#167)
    * status challenge fixed
    
    * back to proposal in proposal view
    
    * status add to problem
    
    * foundation updated
    
    * merged with master
    
    * filter by status
    
    * new structure challenge admin
    
    * design events in admin panel ok

---
+++ app/controllers/admin/design_events_controller.rb
@@ -0,0 +1,67 @@
+class Admin::DesignEventsController < Admin::BaseController
+  load_and_authorize_resource
+
+    def index
+      @problem = Problem.find(params["problem_id"])
+      @project = Project.find(params["project_id"])
+      @design_events = DesignEvent.where(project: @project)
+    end
+
+    def show
+      @problem = Problem.find(params["problem_id"])
+      @project = Project.find(params["project_id"])
+    end
+
+
+    def edit
+      @problem = Problem.find(params["problem_id"])
+      @project = Project.find(params["project_id"])
+    end
+
+    def update
+      @project = Project.find(params["project_id"])
+      @problem = Problem.find(params["problem_id"])
+      if @design_event.save
+        p 'hola'
+        redirect_to admin_project_design_events_path(@project, :problem_id => @problem), notice: "Se han guardado correctamente los cambios"
+      else
+        render :new
+      end
+    end
+
+    def create
+      @project = Project.find(params["project_id"])
+      @problem = Problem.find(params["problem_id"])
+      if @design_event.save
+        redirect_to admin_project_design_events_path(@project, :problem_id => @problem), notice: "Se han guardado correctamente los cambios"
+      else
+        render :new
+      end
+
+    end
+
+    def new
+      @problem = Problem.find(params["problem_id"])
+      @project = Project.find(params["project_id"])
+    end
+
+    def destroy
+      @project = Project.find(params["project_id"])
+      @problem = Problem.find(params["problem_id"])
+      if @design_event.destroy
+        redirect_to admin_project_design_events_path(@project, :problem_id => @problem), notice: "Se han guardado correctamente los cambios"
+      else
+        render :new
+      end
+    end
+
+private
+
+
+def design_event_params
+  params.require(:design_event).permit(:starts_at, :name, :place, :pax, :id, :description, :summary, :project_id, :document, pictures_attributes: [:id, :_destroy, :image])
+end
+
+
+
+end
